9b74432c57cc64bc23d12c0239e0c8dd7a239058
TCR-Nick <141139646+TCR-Nick@users.noreply.github.com>
2023-10-21 09:45:14 +0100

    updated build settings

--- InkpotDemo/Source/InkpotDemoEditor.Target.cs
+++ InkpotDemo/Source/InkpotDemoEditor.Target.cs
@@ -6,8 +6,8 @@ public class InkpotDemoEditorTarget : TargetRules
 	public InkpotDemoEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+		DefaultBuildSettings = BuildSettingsVersion.V3;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("InkpotDemo");
 		ExtraModuleNames.Add("InkpotDemoEditor");
 	}
